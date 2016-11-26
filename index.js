598fd39a026409889249ebc82a95323e17ce706d
cotne nazarashvili <cotne.95@gmail.com>
2016-11-26 13:44:44 +0400

    test: add eslint

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 #!/usr/bin/env node
 
-var Hjson = require('hjson');
-var fs = require('fs');
-var path = require('path');
+var Hjson = require('hjson')
+var fs = require('fs')
+var path = require('path')
 var format = require('string-format')
-var spawn = require('child_process').spawn;
+var spawn = require('child_process').spawn
 
-var commander = require('commander');
+var commander = require('commander')
 
 commander
-	.version('1.0.0')
-	.arguments('<sftp-config.json>')
-	.action(function (sftpConfigPath) {
-		var sftpConfigString = fs.readFileSync(sftpConfigPath).toString();
-		var sftpConfig = Hjson.parse(sftpConfigString);
+  .version('1.0.0')
+  .arguments('<sftp-config.json>')
+  .action(function (sftpConfigPath) {
+    var sftpConfigString = fs.readFileSync(sftpConfigPath).toString()
+    var sftpConfig = Hjson.parse(sftpConfigString)
 
-		openWinscp({
-			type: sftpConfig.type,
-			host: sftpConfig.host,
-			port: sftpConfig.port,
-			user: sftpConfig.user,
-			password: sftpConfig.password,
-			remote_path: sftpConfig.remote_path,
-			local_path: path.dirname(sftpConfigPath),
-			winscp: 'WinSCP',
-		});
-	})
-	.parse(process.argv);
+    openWinscp({
+      type: sftpConfig.type,
+      host: sftpConfig.host,
+      port: sftpConfig.port,
+      user: sftpConfig.user,
+      password: sftpConfig.password,
+      remote_path: sftpConfig.remote_path,
+      local_path: path.dirname(sftpConfigPath),
+      winscp: 'WinSCP'
+    })
+  })
+  .parse(process.argv)
 
-function openWinscp(opts) {
-	var command = '"{winscp}" {type}://"{user}":"{password}"@{host}:{port}"{remote_path}" /rawsettings LocalDirectory="{local_path}"';
-	spawn('sh', ['-c', format(command, opts)], {detached: true}).unref();
-	process.exit();
+function openWinscp (opts) {
+  var command = '"{winscp}" {type}://"{user}":"{password}"@{host}:{port}"{remote_path}" /rawsettings LocalDirectory="{local_path}"'
+  spawn('sh', ['-c', format(command, opts)], {detached: true}).unref()
+  process.exit()
 }
 
-module.exports = openWinscp;
+module.exports = openWinscp
